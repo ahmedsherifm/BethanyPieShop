3a98f21114a9e3683dd94b39b9b525ed72cf1e0e
Ahmed Sherif <ahmed.sherif@integrant.com>
2021-03-01 17:11:56 +0200

    add dbcontext

---
+++ BethanysPieShop/Repositories/PieRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Interfaces;
+using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Repositories
+{
+    public class PieRepository: IPieRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PieRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> AllPies => _appDbContext.Pies.Include(pie=>pie.Category);
+        public IEnumerable<Pie> PiesOfTheWeek => _appDbContext.Pies.Include(pie => pie.Category)
+                                                                   .Where(pie => pie.IsPieOfTheWeek);
+
+        public Pie GetPieById(int pieId) =>
+            _appDbContext.Pies.Include(pie => pie.Category)
+                              .FirstOrDefault(pie => pie.PieId == pieId);
+    }
+}
